6841626f71f60b50605c1f24b2396e3087e5f62f
mella-kais <mallah.kais@gmail.com>
2020-05-14 14:06:19 +0100

    fisnish basic

--- main.js
+++ main.js
@@ -67,8 +67,9 @@ var cardsArray = [
  
  var cardrot = [];
  var cardsArrayId = [];
-
+ var cardMatched = []
 var template = document.querySelector("#template")
+var score = document.querySelector("#score")
 
 var showTemplate = function (){
 	for(var i = 0; i < cardsArray.length; i++){
@@ -80,15 +81,36 @@ var showTemplate = function (){
 	}
 }
 
-function rotateCard (){
+var rotateCard = function  (){
 	var cardId = this.getAttribute('data-id');
 	cardrot.push(cardsArray[cardId].name)
 	cardsArrayId.push(cardId)
 	this.setAttribute('src', cardsArray[cardId].img)
 	 if(cardrot.length === 2 ){
-	 	setTimeout(checkTowCard, 500)
+	 	setTimeout(checkTwoCard, 500)
 
 	 }
+}
+
+var checkTwoCard = function () {
+	var card = document.querySelectorAll('img')
+	var cardOne = cardsArrayId[0];
+	var cardTwo = cardsArrayId[1];
+	console.log(card[cardOne])
+	if(cardrot[0] !== cardrot[1]){
+		card[cardOne].setAttribute('src','images/back.jpg')
+		card[cardTwo].setAttribute('src','images/back.jpg')
+		
+	} else {
+		cardMatched.push(cardrot)
+		
+	}
+	cardrot = [];
+ 	cardsArrayId = [];
+ 	score.textContent = cardMatched.length;
+ 	if(cardMatched.length === cardsArray.length/2){
+ 		score.textContent = "Bravo !!";
+ 	}
 }
 
 
